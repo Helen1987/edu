70b6af3f011a739d579979166911533c2c155eac
Helen1987 <kleit@tut.by>
2012-09-29 22:15:29 +0300

    [+] WcfService PerCall was added

---
+++ .NET/WCF/!My/WCF/Chapter4/Chapter4/Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Client
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var proxy = new MyContractClient())
+			{
+				proxy.MyMethod();
+				proxy.MyMethod();
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ .NET/WCF/!My/WCF/Chapter4/Chapter4/WcfServicePerCall/MyService.svc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Diagnostics;
+
+
+namespace WcfServicePerCall
+{
+	[ServiceBehavior(InstanceContextMode=InstanceContextMode.PerCall)]
+	public class MyService : IMyContract
+	{
+		int counter = 0;
+
+		public MyService()
+		{
+			
+			Trace.WriteLine("MyService.MyService()");
+			
+		}
+
+		public void MyMethod(Param stateIdentifier)
+		{
+			GetSate(stateIdentifier);
+			counter++;
+			Trace.WriteLine("Counter = " + counter);
+			SaveState(stateIdentifier);
+		}
+
+		public void Dispose()
+		{
+			Trace.WriteLine("MyService.Dispose()");
+		}
+
+		void GetSate(Param stateIdentifier)
+		{ }
+
+		void SaveState(Param stateIdentifier)
+		{ }
+	}
+}
