3ad79ef1c6bb5123e5130e7aebd00582826404d0
Helen1987 <kleit@tut.by>
2012-09-30 17:04:16 +0300

    [+] Service with demarcating operations was added

---
+++ .NET/WCF/!My/WCF/Chapter4/Chapter4/WcfServiceDemarcation/IOrderManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceDemarcation
+{
+	[ServiceContract(SessionMode=SessionMode.Required)]
+	public interface IOrderManager
+	{
+		[OperationContract]
+		void SetCustomerId(int customerId);
+
+		[OperationContract(IsInitiating=false)]
+		void AddItem(int itemId);
+
+		[OperationContract(IsInitiating=false)]
+		decimal GetTotal();
+
+		[OperationContract(IsInitiating = false, IsTerminating = true)]
+		bool ProcessOrders();
+	}
+}
