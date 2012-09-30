70aacbb76af795a97d377ebecc70734206b83a8f
Helen1987 <kleit@tut.by>
2012-09-30 16:42:58 +0300

    [+] ServiceHost<T> was added

---
+++ .NET/WCF/!My/WCF/Chapter4/Chapter4/Enhancements/InProcFactory.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Diagnostics;
+
+namespace Enhancements.Factory
+{
+	public static class InProcFactory
+	{
+		struct HostRecord
+		{
+			public ServiceHost Host { get; private set; }
+			public string Address { get; private set; }
+
+			public HostRecord(ServiceHost host, string address) : this()
+			{
+				Host = host;
+				Address = address;
+			}
+		}
+
+		static readonly Uri BaseAddress = new Uri("net.pipe://localhost/");
+		static readonly Binding NamedPipeBinding;
+		static Dictionary<Type, HostRecord> _hosts = new Dictionary<Type, HostRecord>();
+
+		static InProcFactory()
+		{
+			NamedPipeBinding = new NetNamedPipeBinding { TransactionFlow = true };
+			AppDomain.CurrentDomain.ProcessExit += ((p1, p2) =>
+					{
+						foreach (var pair in _hosts)
+							pair.Value.Host.Close();
+					});
+		}
+
+		public static I CreateInstanse<S, I>()
+			where I : class
+			where S : I
+		{
+			var hostRecord = GetHostRecord<S, I>();
+			return ChannelFactory<I>.CreateChannel(NamedPipeBinding, new EndpointAddress(hostRecord.Address));
+		}
+
+		private static HostRecord GetHostRecord<S, I>() where I : class
+													where S : I
+		{
+			HostRecord hostRecord;
+			if (_hosts.ContainsKey(typeof(S)))
+			{
+				hostRecord = _hosts[typeof(S)];
+			}
+			else
+			{
+				var host = new ServiceHost(typeof(S), BaseAddress);
+				string address = BaseAddress.ToString() + Guid.NewGuid().ToString();
+				hostRecord = new HostRecord(host, address);
+				_hosts.Add(typeof(S), hostRecord);
+				host.AddServiceEndpoint(typeof(I), NamedPipeBinding, address);
+				host.Open();
+			}
+			return hostRecord;
+		}
+
+		public static void CloseProxy<I>(I instance) where I : class
+		{
+			var proxy = instance as ICommunicationObject;
+			Debug.Assert(proxy != null);
+			proxy.Close();
+		}
+	}
+}
---
+++ .NET/WCF/!My/WCF/Chapter4/Chapter4/Enhancements/ServiceHost.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Diagnostics;
+using System.ServiceModel.Channels;
+
+namespace Enhancements
+{
+	public class ServiceHost<T> : ServiceHost
+	{
+		public ServiceHost() : base(typeof(T)) { }
+
+		public ServiceHost(params string[] baseAddresses) :
+			base(typeof(T), Convert(baseAddresses)) { }
+
+		public ServiceHost(params Uri[] baseAddress) :
+			base(typeof(T), baseAddress) { }
+
+		// for singleton instance
+		public ServiceHost(T singleton, params Uri[] baseAddresses) : base(singleton, baseAddresses) { }
+
+		public virtual T Singleton
+		{
+			get
+			{
+				if (SingletonInstance == null)
+				{
+					return default(T);
+				}
+				return (T)SingletonInstance;
+			}
+		}
+
+		private static Uri[] Convert(string[] baseAddresses)
+		{
+			return baseAddresses.Select(elem => new Uri(elem)).ToArray();
+		}
+
+		public bool EnableMetadataExchange
+		{
+			set
+			{
+				if (State == CommunicationState.Opened)
+				{
+					throw new InvalidOperationException("Host is already opened");
+				}
+				var metadataBehavior = Description.Behaviors.Find<ServiceMetadataBehavior>();
+				if (metadataBehavior == null)
+				{
+					metadataBehavior = new ServiceMetadataBehavior();
+					Description.Behaviors.Add(metadataBehavior);
+				}
+				if (value == true)
+				{
+					if (HasMexEndpoint == false)
+					{
+						AddAllMexEndpoints();
+					}
+				}
+			}
+			get
+			{
+				var metadataBehavior = Description.Behaviors.Find<ServiceMetadataBehavior>();
+				return metadataBehavior == null ? false : metadataBehavior.HttpGetEnabled;
+			}
+		}
+
+		public bool HasMexEndpoint
+		{
+			get
+			{
+				return Description.Endpoints.Any(endpoint =>
+					endpoint.Contract.ContractType == typeof(IMetadataExchange));
+			}
+		}
+
+		public void AddAllMexEndpoints()
+		{
+			Debug.Assert(HasMexEndpoint == false);
+
+			foreach (var baseAddress in BaseAddresses)
+			{
+				BindingElement bindingElement = null;
+				switch (baseAddress.Scheme)
+				{
+					case "net.tcp":
+						{
+							bindingElement = new TcpTransportBindingElement();
+							break;
+						}
+					case "net.pipe":
+						{
+							bindingElement = new NamedPipeTransportBindingElement();
+							break;
+						}
+					case "http":
+						{
+							bindingElement = new HttpTransportBindingElement();
+							break;
+						}
+					case "https":
+						{
+							bindingElement = new HttpsTransportBindingElement();
+							break;
+						}
+				}
+				if (bindingElement != null)
+				{
+					var binding = new CustomBinding(bindingElement);
+					AddServiceEndpoint(typeof(IMetadataExchange), binding, "MEX");
+				}
+			}
+		}
+	}
+}
