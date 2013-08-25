dc75fdb070ea954f9ff576b6be71a27f3623d52c
Helen1987 <kleit@tut.by>
2013-08-25 19:13:30 +0300

    Richter: Adding Chapter 5

---
+++ .NET/Рихтер/Chapter5/Chapter5/BoxingUnboxing.cs
@@ -0,0 +1,28 @@
+﻿#define INEFFICIENT
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter5
+{
+	class BoxingUnboxing
+	{
+		public void Write()
+		{
+			int v = 5;
+
+#if INEFFICIENT
+			// When compiling the following code line, v is boxed
+			// three times, wasting time and memory
+			Console.WriteLine("{0}, {1}, {2}", v, v, v);
+#else
+			// The lines below have the same result, execute
+			// much faster, and use less memory
+			Object o = v;
+
+			Console.WriteLine("{0}, {1}, {2}", o, o, o);
+#endif
+		}
+	}
+}
---
+++ .NET/Рихтер/Chapter5/Chapter5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter5
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var boxing = new BoxingUnboxing();
+			boxing.Write();
+
+			Console.ReadKey();
+		}
+	}
+}
