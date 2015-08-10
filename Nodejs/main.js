fb0eab7c6e9d344597bcfa1818ef0bfdc2136595
Helen1987 <kleit@tut.by>
2015-08-10 23:02:51 +0300

    Add mongoose

--- Nodejs/main.js
+++ Nodejs/main.js
@@ -1,13 +1,80 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+//mongoose uses Schema as a layer on a MongoDb document (which represents a model):
+var Schema = mongoose.Schema;
 
-app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
-var menu = [{ "description": "papadums", "price": "2,00" }, { "description": "chicken tikka masala", "price": "14,00" },
-{ "description": "fisher king beer", "price": "2,50" }, { "description": "sag paneer", "price": "6,00" }];
+//connect to mongodb:
+mongoose.connect('mongodb://localhost:27017/restaurant');
 
+//create a Schema for our food:
+var FoodSchema = new Schema({
+    name: {
+        type: String,
+        index: {
+            unique: true
+        }
+    },
+    description: String,
+    price: String
+});
+
+
+// Use the schema to register a model with MongoDb
+mongoose.model('Food', FoodSchema);
+var food = mongoose.model('Food');
+
+//POST verb
+app.post('/food', function (req, res) {
+    food.create(req.body, function (err, food) {
+        if (err) {
+            res.send(401, err);
+            return;
+        }
+        res.send(req.body);
+    });
+});
+
+//GET verb
 app.get('/food', function (req, res) {
-    res.send(menu);
+    food.find(function (err, data) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(data);
+    });
+});
+
+//GET/id
+app.get('/food/:id', function (req, res) {
+    food.findOne({
+        _id: req.params.id
+    }, function (error, response) {
+        if (error) {
+            res.send(error);
+        } else {
+            res.send(response);
+        }
+    });
+});
+
+//GET by name
+app.get('/foodname/:name', function (req, res) {
+    food.findOne({
+        name: req.params.name
+    }, function (error, response) {
+        if (error || !response) {
+            res.send("not on the menu");
+        } else {
+            res.send(response);
+        }
+    });
 });
 
 app.set('port', process.env.PORT || 3000);
